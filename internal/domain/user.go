5142d4356f3aa7fd56d436a5d91931adfb5858a0
Saidakmal Oktamov <saidokma.228@gmail.com>
2026-03-27 00:38:20 +0500

    feat: add Language, XP, Level, StreakShields, EveningRecapHour to User domain

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -3,10 +3,20 @@ package domain
 import "time"
 
 type User struct {
-	ID         int64
-	TelegramID int64
-	Username   string
-	FirstName  string
-	Timezone   string
-	CreatedAt  time.Time
+	ID               int64
+	TelegramID       int64
+	Username         string
+	FirstName        string
+	Timezone         string
+	Language         string
+	XP               int
+	Level            int
+	StreakShields    int
+	EveningRecapHour int
+	CreatedAt        time.Time
+}
+
+type UserAchievement struct {
+	Code       string
+	UnlockedAt time.Time
 }
